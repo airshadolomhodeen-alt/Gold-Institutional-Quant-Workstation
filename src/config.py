aa67181101badbb78fecef5df667d6a29ffd75a7
airshadolomhodeen-alt <airshadolomhodeen@gmail.com>
2026-09-24 11:36:13 +0800

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Central Configuration Dataclass
+"""
+class Config:
+    RSI_PERIOD = 14
+    MACD_FAST = 12
+    MACD_SLOW = 26
+    ATR_PERIOD = 14
+    EWMA_LAMBDA = 0.94
