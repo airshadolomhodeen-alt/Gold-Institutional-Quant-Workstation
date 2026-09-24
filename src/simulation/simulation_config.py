561feebf7aff892d4cd42eb850706c582d1a000d
airshadolomhodeen-alt <airshadolomhodeen@gmail.com>
2026-09-25 06:41:38 +0800

    Create simulation_config.py

---
+++ src/simulation/simulation_config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Simulation Configuration Parameters"""
+class SimulationConfig:
+    N_TOTAL = 10000
+    STAGE_1_RATIO = 0.60
+    STAGE_2_RATIO = 0.20
+    STAGE_3_RATIO = 0.20
+    PARTITION_MODE = "chronological"
+    SIMULATION_SEED = 42
+    TOP_K_ENTRIES = 50
+    MIN_SAMPLE_SIZE_PER_CASE = 30
+    MIN_PROFIT_RATE_THRESHOLD = 0.002
